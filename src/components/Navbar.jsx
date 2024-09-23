e18fe2676f235a6f3ccb6e587b822780b7e04dda
iamadarsh1994 <iamadarsh1994@gmail.com>
2024-09-23 13:20:43 +0530

    updated

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+function Navbar() {
+    return (
+      <div className="bg-black text-white flex justify-center items-center rounded-2xl hover:bg-slate-400 p-4 m-4">
+  <div><p className="test-2xl flex justify-center items-center">Hello</p>
+  <p className="animate slideRight test-2xl text-yellow-400">Click on the below button to change the background Color of the below body</p></div>      
+      </div>
+    ) 
+  }
+  
+  export default Navbar
